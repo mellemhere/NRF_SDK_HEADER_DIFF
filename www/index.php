df308a5ff6975255b145a76f9fb5dc54f1ba4e85
gmellem <gmellem@wiley.com>
2021-02-03 21:13:58 -0300

    Fix bug #2

--- www/index.php
+++ www/index.php
@@ -17,55 +17,58 @@ $diferenciado = 'DIFERENCIADO';
 if (!empty($_FILES)) {
     $ar_certo = $_FILES['sdk_certo']['tmp_name'];
     $ar_difereciado = $_FILES['sdk_diferenciado']['tmp_name'];
-    $compared = true;
 
+    if ($ar_certo && $ar_difereciado) {
 
-    $correta = $_POST['c'];
-    $diferenciado = $_POST['d'];
+        $compared = true;
 
-    $leitura_ar_certo = fopen($ar_certo, "r");
-    $leitura_ar_diferenciado = fopen($ar_difereciado, "r");
+        $correta = $_POST['c'];
+        $diferenciado = $_POST['d'];
 
-    while (($linha = fgets($leitura_ar_certo)) !== false) {
-        if (strpos($linha, "#define") !== false) {
-            $comando = explode(" ", $linha);
+        $leitura_ar_certo = fopen($ar_certo, "r");
+        $leitura_ar_diferenciado = fopen($ar_difereciado, "r");
 
-            if (array_key_exists($comando[1], $definesArquivoCerto)) continue;
-            $definesArquivoCerto[$comando[1]] = trim($comando[2]);
-        }
-    }
+        while (($linha = fgets($leitura_ar_certo)) !== false) {
+            if (strpos($linha, "#define") !== false) {
+                $comando = explode(" ", $linha);
 
-
-    while (($linha = fgets($leitura_ar_diferenciado)) !== false) {
-        if (strpos($linha, "#define") !== false) {
-            $comando = explode(" ", $linha);
-
-            if (array_key_exists($comando[1], $definesArquivoDiferenciado)) continue;
-            $definesArquivoDiferenciado[$comando[1]] = trim($comando[2]);
-        }
-    }
-
-
-    foreach ($definesArquivoCerto as $define => $valor) {
-        if (!array_key_exists($define, $definesArquivoDiferenciado)) {
-            $configuracoesQueTemNoCertoENaoTemNoDiferenciado[$define] = $valor;
-        } else {
-            if ($valor != $definesArquivoDiferenciado[$define]) {
-                $valoresDiferentesEntreCertoEDiferenciado[$define] = [$valor, $definesArquivoDiferenciado[$define]];
+                if (array_key_exists($comando[1], $definesArquivoCerto)) continue;
+                $definesArquivoCerto[$comando[1]] = trim($comando[2]);
             }
         }
-    }
 
 
-    foreach ($definesArquivoDiferenciado as $define => $valor) {
-        if (!array_key_exists($define, $definesArquivoCerto)) {
-            $configuracoesQueTemNoDiferenciadoENaoTemNoCerto[$define] = $valor;
+        while (($linha = fgets($leitura_ar_diferenciado)) !== false) {
+            if (strpos($linha, "#define") !== false) {
+                $comando = explode(" ", $linha);
+
+                if (array_key_exists($comando[1], $definesArquivoDiferenciado)) continue;
+                $definesArquivoDiferenciado[$comando[1]] = trim($comando[2]);
+            }
         }
-    }
 
-    if($leitura_ar_certo && $leitura_ar_diferenciado) {
-        fclose($leitura_ar_certo);
-        fclose($leitura_ar_diferenciado);
+
+        foreach ($definesArquivoCerto as $define => $valor) {
+            if (!array_key_exists($define, $definesArquivoDiferenciado)) {
+                $configuracoesQueTemNoCertoENaoTemNoDiferenciado[$define] = $valor;
+            } else {
+                if ($valor != $definesArquivoDiferenciado[$define]) {
+                    $valoresDiferentesEntreCertoEDiferenciado[$define] = [$valor, $definesArquivoDiferenciado[$define]];
+                }
+            }
+        }
+
+
+        foreach ($definesArquivoDiferenciado as $define => $valor) {
+            if (!array_key_exists($define, $definesArquivoCerto)) {
+                $configuracoesQueTemNoDiferenciadoENaoTemNoCerto[$define] = $valor;
+            }
+        }
+
+        if ($leitura_ar_certo && $leitura_ar_diferenciado) {
+            fclose($leitura_ar_certo);
+            fclose($leitura_ar_diferenciado);
+        }
     }
 }
 ?>
@@ -107,11 +110,13 @@ if (!empty($_FILES)) {
         <div class="card-body">
             <form method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label for="exampleInputEmail1">SDK <input type="text" class="form-control inline-input" value="<?= $correta ?>" name="c"></label>
+                    <label for="exampleInputEmail1">SDK <input type="text" class="form-control inline-input"
+                                                               value="<?= $correta ?>" name="c"></label>
                     <input type="file" class="form-control" name="sdk_certo">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">SDK <input type="text" class="form-control inline-input" value="<?= $diferenciado ?>" name="d"></label>
+                    <label for="exampleInputPassword1">SDK <input type="text" class="form-control inline-input"
+                                                                  value="<?= $diferenciado ?>" name="d"></label>
                     <input type="file" class="form-control" name="sdk_diferenciado">
                 </div>
                 <button type="submit" class="btn btn-primary">COMPARAR</button>
@@ -166,8 +171,8 @@ if (!empty($_FILES)) {
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">CONFIGURAÇÃO</th>
-                    <th scope="col">VALOR CORRETO</th>
-                    <th scope="col">VALOR DIFERENTE</th>
+                    <th scope="col">VALOR <?= $correta ?></th>
+                    <th scope="col">VALOR <?= $diferenciado ?></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -191,7 +196,8 @@ if (!empty($_FILES)) {
 
             <hr style="margin: 50px 0px 50px 0px;">
             <h2>Itens que estão no <span
-                        class="badge badge-danger"><?= $diferenciado ?></span> e não no <span class="badge badge-success"><?= $correta ?></span>.
+                        class="badge badge-danger"><?= $diferenciado ?></span> e não no <span
+                        class="badge badge-success"><?= $correta ?></span>.
                 Qtd: <?= count($configuracoesQueTemNoDiferenciadoENaoTemNoCerto) ?></h2>
             <table class="table">
                 <thead class="thead-dark">
